e1206f90648fe3e91777aa9075367ff748e94801
Shubham kahar <Shubhamkahar196@gmail.com>
2026-01-02 21:54:17 +0530

    Structure the folder

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../Navbar/Navbar"
+import Education from "./Education"
+import Hero from "./Hero"
+import Pricing from "./Pricing"
+import Stats from "./Stats"
+
+
+
+const Home = ()=>{
+    return <>
+      <Navbar/>
+      <Hero/>
+      <Pricing/>
+      <Stats/>
+      <Education/>
+
+    </>
+}
+
+export default Home
