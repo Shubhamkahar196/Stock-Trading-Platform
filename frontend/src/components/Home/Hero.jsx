4fd6ac2d147c7a3657d65ddd7da0dfee48a20372
Shubham kahar <Shubhamkahar196@gmail.com>
2026-01-03 23:23:21 +0530

    completed hero components

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -2,8 +2,17 @@
 
 const Hero = ()=>{
     return (
-        <div className="flex justify-center items-center mt-5 px-5">
-            <img src="https://zerodha.com/static/images/landing.svg" alt="" className="max-w-full h-auto" />
+        <div className="flex flex-col justify-center items-center mt-5 px-5">
+            <img src="https://zerodha.com/static/images/landing.svg" alt="Stock trading platform hero image" className="max-w-full h-87.5 mb-5" />
+
+            <div className="mt-5 flex flex-col text-center">
+                <h2 className="text-3xl font-semi mb-4">Invest in everything</h2>
+                <p className="text-lg mb-6">Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+            </div>
+
+            <div className="mt-5">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign up for Free</button>
+            </div>
         </div>
     )
 }
